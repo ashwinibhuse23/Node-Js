bac5cd48477d469cb23551fd0e1a47c5a227f588
Ashwini Bhuse <163240898+ashwinibhuse23@users.noreply.github.com>
2024-05-30 13:22:35 +0530

    Add files via upload
    E-mail

---
+++ apps.js
@@ -0,0 +1,27 @@
+//nuarfcybzzxuojyu
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+ service : "gmail",
+  auth: {
+    user: "ashwini.bhuse09@gmail.com",
+    pass: "nuarfcybzzxuojyu",
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: "<ashwini.bhuse09@gmail.com>", // sender address
+    to: "divpawar17@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
